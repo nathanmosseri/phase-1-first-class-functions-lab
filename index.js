d8f78ef403719d43e335752adc677fd7ceee7d05
Nathan <namo678@me.com>
2022-07-20 10:59:09 -0400

    commit tree e2a269277db1aecc9f64d4713e4f37a157868cd4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+const drivers = ['kenny', 'kyle', 'david', 'frank', 'joe', 'larry', 'lenny']
+
+//anon function takes in an array
+const returnFirstTwoDrivers = (array) => {
+//return first two drivers as array
+   return array.slice(0,2)
+}
+
+const returnLastTwoDrivers = (array) => {
+    return array.slice(array.length - 2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+//having () rund the function we just want to store it 
+
+//function takes in an integer, multiplier
+const createFareMultiplier = (multiplier) => {
+    //return anon function
+    //function takes parameter fare (integer)
+    return function(fare) {
+        //multiply fare by multiplier
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+/*
+fareDoubler = 
+const createFareMultiplier = (multiplier) => {
+    return function(fare) {
+        return fare * multiplier
+    }
+} 
+*/
+
+const fareTripler = createFareMultiplier(3)
+
+//function takes in an array, function
+const selectDifferentDrivers = (array, func) => {
+    return func(array)
+}
